8aee8e15bf1a2cb670b1148c22a122d9f8dea1be
Antoni Realp Serrano <antoni.realp@enti.cat>
2018-02-19 19:49:50 +0100

    Version 1.0

---
+++ Juegito_the_game/Assets/scripts/Traspassing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traspassing : MonoBehaviour {
+
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Player = collision.gameObject;
+        }
+    }
+
+    IEnumerator WaitForCollider()
+    {
+        // suspend execution for 1 seconds
+        yield return new WaitForSeconds(0.2f);
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), Player.GetComponent<Collider2D>(), false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+       
+        if (Input.GetButtonDown("XboxA") && Input.GetAxis("LeftJoyY") == -1)
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), Player.GetComponent<Collider2D>(), true);
+
+            StartCoroutine(WaitForCollider());
+        }
+    }
+}
