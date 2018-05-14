6d47c62541098ea548ce735067b70b54eb1cf27a
Arnau Aguilar <arnau.aguilar@enti.cat>
2018-05-14 17:43:16 +0200

    Space map

--- Juegito_the_game/Assets/scripts/PlatformMovment.cs
+++ Juegito_the_game/Assets/scripts/PlatformMovment.cs
@@ -6,20 +6,20 @@ public class PlatformMovment : MonoBehaviour {
     public float timer;
     private float currentT;
     public GameObject PlatformPrefab;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         currentT += Time.deltaTime;
-        if(currentT >= timer) {
+        if (currentT >= timer)
+        {
             GameObject platform = (GameObject)Instantiate(PlatformPrefab, new Vector3(-10, -1.5f, 0), Quaternion.Euler(0, 0, 90));
             platform.GetComponent<Rigidbody2D>().velocity = new Vector3(1, 0, 0);
             currentT = 0;
 
         }
-		
 	}
 }
