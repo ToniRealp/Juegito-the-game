6e88ebde5d3f4214d1bc976a8450877116653ba5
Antoni Realp Serrano <antoni.realp@enti.cat>
2018-05-16 00:16:20 +0200

    shoot particle effect

---
+++ Juegito_the_game/Assets/scripts/ParticleManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour {
+
+	public ParticleSystem shootExplosion;
+	void Start () {
+
+        shootExplosion = GetComponentInChildren<ParticleSystem>();
+
+	}
+	
+	
+	void Update () {
+		
+       
+
+	}
+}
