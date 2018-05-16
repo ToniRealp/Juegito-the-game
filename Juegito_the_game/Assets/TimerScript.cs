ac58317299ae006b5a63b1ecd7db457afd322d97
Unknown <fernando.gonzalez@enti.cat>
2018-05-16 13:24:48 +0200

    Timer, Sudden Death
    :D

--- Juegito_the_game/Assets/TimerScript.cs
+++ Juegito_the_game/Assets/TimerScript.cs
@@ -8,13 +8,37 @@ public class TimerScript : MonoBehaviour {
 
     float timer;
     public GameController gC;
+    int minutes;
+    int seconds;
 	
 	void Update () {
 
 
         timer = gC.gameTimer;
-        GetComponent<TextMeshProUGUI>().text = timer.ToString();
 
+        minutes = (int)timer / 60;
+        seconds = (int)timer % 60;
 
-	}
+        if (gC.suddenDeath == false)
+        {
+
+            if (timer > 0 && !gC.gameOver)
+            {
+
+                if (seconds > 9)
+                    GetComponent<TextMeshProUGUI>().text = minutes.ToString() + ":" + seconds.ToString();
+
+                if (seconds <= 9)
+                    GetComponent<TextMeshProUGUI>().text = minutes.ToString() + ":0" + seconds.ToString();
+
+            }
+            else if (timer <= 0 || gC.gameOver)
+            {
+                GetComponent<TextMeshProUGUI>().text = "FINISH!";
+            }
+        } else
+        {
+            GetComponent<TextMeshProUGUI>().text = "SUDDEN DEATH!!!";
+        }
+    }
 }
